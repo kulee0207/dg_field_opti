f592c9f44c3771602cd7ea27d293d5383c5fce0f
이강욱님/대구Access Infra팀 <1113185@sktelecom.com>
2025-04-24 17:34:17 +0900

    test code change

--- pages/test.py
+++ pages/test.py
@@ -18,7 +18,7 @@ dec_31 = datetime.date(this_year, 12, 31)
 
 d = st.date_input(
     "Select your vacation for next year",
-    (jan_1, datetime.date(next_year, 1, 7)),
+    (jan_1, datetime.date(this_year, 1, 7)),
     jan_1,
     dec_31,
     format="MM.DD.YYYY",
